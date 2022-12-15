8bfb4669fad93eddc1f3475d05489ea4043d1cac
Farid F. Utama <48374273+faridfirdautama@users.noreply.github.com>
2022-12-15 20:14:38 +0700

    refix ajax for admin and member

--- app/views/admin/footer.php
+++ app/views/admin/footer.php
@@ -24,11 +24,10 @@
 <script src="https://cdn.datatables.net/responsive/2.2.4/js/dataTables.responsive.min.js"></script>
 <script src="https://cdn.datatables.net/responsive/2.2.4/js/responsive.bootstrap4.min.js"></script>
 <script src="<?= BASEURL ?>/js/admin-ajax.js"></script>
+<script src="<?= BASEURL ?>/js/member-ajax.js"></script>
 <script type="text/javascript">
     $('#daftar-pinjaman').DataTable();
     $('#tbl-daftar-barang').DataTable();
-    $('#tbl-list-merek').DataTable();
-    $('#tbl-list-kategori').DataTable();
 </script>
 
 <!-- Custom scripts for all pages-->
